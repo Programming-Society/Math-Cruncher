31ff1162b130398a435a5636a383b332f630b18b
SusanaSant <susanasantiesteban@gmail.com>
2021-10-15 15:18:48 -0400

    Susana's attempt at a primitive menu

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 
 
 # display menu and process menu options
+# display choices to users depending on if user is logged in.
+# If activeUser="" then only show login and quit options.
+# If activeUser!="" then welcome logged in user and display logout and menu options.
+# Validate user input and return user choice
+# menu options: login/logout, play game, display scores, quit
+def Menu ():
+    activeuser = ""
+    activeuser = input("Enter your username: ")
+    if (activeuser != ""):
+        menu = ["
+        print("Welcome user" + activeuser)
+    else:
+        print()
 def main():
-
     return 1;
-
+if __name__ == '__main__':
+    main()
 
 
